2ff039bdc2f624b751ff7006bd5c18966aa650cb
Sanaz <s.shahraeini@gmail.com>
2024-06-01 12:44:38 +0330

    add .env file

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import requests
 
-TOKEN = "7030511458:AAHHTGapgMb5_b7YDvySnDts2OC-LwUkWyA"
-CHAT_ID = "80088917"
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values
+# loading variables from .env file
+load_dotenv()
+
+# accessing and printing value
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 # url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 # print(requests.get(url).json())
-message = "Hello from pycharm, How r u?"
+message = "Hello I am your TeleBot, Nice to meet you :-)"
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
 res = requests.get(url)
 print(res)
